6c80a8012a6c49dad4e1c32d4a1c346b99da72e0
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-12 16:28:51 -0500

    Add public private and readonly access modifiers

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 // classes
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    //   readonly client: string;
+    //   private details: string;
+    //   public amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes €${this.amount} for ${this.details}`;
@@ -15,6 +18,9 @@ const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
+invoices.forEach((inv) => {
+    console.log(inv.client, inv.details, inv.amount, inv.format());
+});
 const form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 // classes
 
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+//   readonly client: string;
+//   private details: string;
+//   public amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number,
+  ){}
 
   format() {
     return `${this.client} owes €${this.amount} for ${this.details}`;
@@ -22,6 +22,10 @@ const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.details, inv.amount, inv.format());
+});
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
