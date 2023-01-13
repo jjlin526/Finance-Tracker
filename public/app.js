d6b94d1e517150dc22e0f1c2b12a92d824623f07
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-13 16:35:28 -0500

    Enable users to submit invoice/payment info and log it to console as an object

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
 // const me: isPerson = {
 //   name: "jonathan",
 //   age: 22,
@@ -31,5 +32,12 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
